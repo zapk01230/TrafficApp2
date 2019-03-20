3bb632ddfcf43c03e407c650e2adacdd34b1c08e
Eakkapod <zaeak.123@gmail.com>
2019-03-20 10:30:53 +0700

    Design Main UI

---
+++ app/src/main/java/sdu/cs/eakkapod/trafficapp/MainActivity.java
@@ -0,0 +1,13 @@
+package sdu.cs.eakkapod.trafficapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
